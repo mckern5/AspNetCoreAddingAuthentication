def37d12639c5e5badae4fabe767cb9de534cf96
Matt Kern <matt.kern@hennigesautomotive.com>
2021-05-10 16:17:27 -0400

    completed Module3 Create AccountController

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WishList.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace WishList.Controllers
+{   
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        private AccountController(UserManager<ApplicationUser> userMan, SignInManager<ApplicationUser> signMan)
+        {
+            _userManager = userMan;
+            _signInManager = signMan;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

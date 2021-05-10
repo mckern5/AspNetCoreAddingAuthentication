ec89ef334fa067cd1df38c59417397ea89954615
Matt Kern <matt.kern@hennigesautomotive.com>
2021-05-10 17:27:55 -0400

    Create RegisterVIewModel Class

---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        String email { get; set; };
+
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        String Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        String ConfirmPassword { get; set; }
+
+    }
+}
